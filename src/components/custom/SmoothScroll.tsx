2a1755d27c0cab4fa2888c96fa55d9815f02b247
Shahbaz Khan <shahbazkhan19111@gmail.com>
2025-10-01 16:12:39 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/components/custom/SmoothScroll.tsx
+++ src/components/custom/SmoothScroll.tsx
@@ -1,33 +1,34 @@
 
 'use client'
 import { ReactLenis, useLenis } from '@studio-freight/react-lenis'
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 export function SmoothScroll({ children }: { children: React.ReactNode }) {
-    const lenisRef = useRef<any>(null)
-    useEffect(() => {
-        if(lenisRef.current) {
-            lenisRef.current.lenis.on('scroll', ScrollTrigger.update)
-            gsap.ticker.add((time: any)=>{
-                lenisRef.current.lenis.raf(time * 1000)
-            })
-            gsap.ticker.lagSmoothing(0)
-        }
+  const lenis = useLenis(({ scroll }) => {
+    ScrollTrigger.update();
+  })
 
-        return () => {
-            if(lenisRef.current) {
-                // clean up listeners and ticker
-                 gsap.ticker.remove((time: any) => {
-                    lenisRef.current.lenis.raf(time * 1000)
-                })
-            }
-        }
-    }, [])
+  useEffect(() => {
+    if (lenis) {
+      gsap.ticker.add((time)=>{
+          lenis.raf(time * 1000)
+      })
+      gsap.ticker.lagSmoothing(0)
+    }
+
+    return () => {
+       if (lenis) {
+         gsap.ticker.remove((time: any) => {
+            lenis.raf(time * 1000)
+        })
+       }
+    }
+  }, [lenis])
 
   return (
-    <ReactLenis root ref={lenisRef} autoRaf={false}>
+    <ReactLenis root autoRaf={false}>
       {children}
     </ReactLenis>
   )
