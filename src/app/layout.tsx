3d7831377d3ecf90416ee690ce0e5a4dd2b9c267
Shahbaz Khan <shahbazkhan19111@gmail.com>
2025-10-02 15:53:45 +0000

    change title Khan motor to ArmanAutoxperts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import { FirebaseProvider } from '@/firebase/provider';
 import { FirebaseErrorListener } from '@/components/FirebaseErrorListener';
 
 export const metadata: Metadata = {
-  title: 'Khan Motor',
+  title: 'ArmanAutoxperts',
   description: 'Explore premium vehicles, contact us, or use our AI tools. All in one place.',
 };
 
@@ -52,4 +52,3 @@ export default function RootLayout({
     </html>
   );
 }
-    
