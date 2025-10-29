a587851badd561eb6274417342d301fe5bec49d5
Shahbaz Khan <shahbazkhan19111@gmail.com>
2025-10-29 14:17:55 +0000

    what our client say  in mbile view is not bold like other section headin

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 import React from "react";
 import {
@@ -175,7 +174,7 @@ export const HeroParallax = ({
 export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
-      <h1 className="text-2xl md:text-7xl font-bold dark:text-white uppercase">
+      <h1 className="text-2xl md:text-7xl font-black dark:text-white uppercase">
         What Our Clients Say
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200 lowercase">
