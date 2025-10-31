4447ae09f613dbc93f29b8c1047aadca26c03794
Shahbaz Khan <shahbazkhan19111@gmail.com>
2025-10-31 06:44:51 +0000

    i want to create a robot txt for seo index ✅ robots.txt for motorkhan.c

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,16 @@
 module.exports = {
   siteUrl: 'https://motorkhan.com',
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: '*',
+        disallow: ['/admin/', '/api/', '/server/'],
+      }
+    ]
+  }
 };
