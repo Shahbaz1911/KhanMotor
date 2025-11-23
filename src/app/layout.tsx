6a6ea9bb27376de2563eeaa8a84d6c8c3289611f
Shahbaz Khan <shahbazkhan19111@gmail.com>
2025-11-23 09:38:57 +0000

    <!-- Google Tag Manager --> <script>(function(w,d,s,l,i){w[l]=w[l]||[];w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import { FirebaseErrorListener } from '@/components/FirebaseErrorListener';
 import { CookieConsentBanner } from '@/components/custom/CookieConsentBanner';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
 import { ScrollProgressBar } from '@/components/custom/ScrollProgressBar';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'Motor Khan - Car Denting, Painting & Restoration in Rohini, Rithala, Delhi',
@@ -121,6 +122,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W7F348D7');`}}></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
@@ -137,6 +143,8 @@ export default function RootLayout({
       </head>
 
       <body className="font-body antialiased">
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W7F348D7"
+height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
         <FirebaseClientProvider>
             <ThemeProvider
               attribute="class"
